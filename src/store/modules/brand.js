d3fd6e6b121120ffdfcc862004b58bc27db364cb
phanvanhieu1998 <phanvanhieu0708@gmail.com>
2021-08-03 17:10:08 +0700

    dfdfdfd

--- src/store/modules/brand.js
+++ src/store/modules/brand.js
@@ -1,94 +1,80 @@
-import Brands from '@/api/brands'
-
+import Brands from "@/api/brands";
 
 const state = {
-  listData:[],
-  totalData:0,
-  limit:10,
-  page:1
-}
+  listData: [],
+  totalData: 0,
+  limit: 4,
+  page: 1
+};
 
 const mutations = {
-  LIST_CATEGORY : (state,res) => state.listData = res,
-  set_page:(state, page) => state.page = page,
-  set_totalData:(state, totalData) => state.totalData = totalData
- 
+  LIST_CATEGORY: (state, res) => (state.listData = res),
+  set_page: (state, page) => (state.page = page),
+  set_totalData: (state, totalData) => (state.totalData = totalData)
+};
 
-}
-
-const getters = {
-  
-}
+const getters = {};
 
 const actions = {
-    add({state,commit},data){
-        console.log(state)
-        return new Promise((resolve,reject) =>{
-    
-          Brands.addBrand(data)
-    
-          .then((res) =>{
-            
-            resolve(res.message)
-      
-          })
-          .catch((error) =>{
-            reject(error)
-          })
-        })
-      },
-    listBrand({state,commit}){
-      return new Promise((resolve,reject) =>{
-        let data = {
-          page:state.page,
-          limit:state.limit
-        }
+  add({ state, commit }, data) {
+    console.log(state);
+    return new Promise((resolve, reject) => {
+      Brands.addBrand(data)
 
-        Brands.listBrand(data).then(res =>{
-          commit('LIST_CATEGORY',res.data.result)
-          commit('set_totalData',res.data.meta.total)
-          resolve(res)
-         
+        .then(res => {
+          resolve(res.message);
         })
-        .catch((error)=>{
-          reject(error) 
-        })
-        
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  listBrand({ state, commit }) {
+    return new Promise((resolve, reject) => {
+      let data = {
+        page: state.page,
+        limit: state.limit
+      };
 
-      })
-    },
-    deleteBrand({ commit },item) {
-      
-      return new Promise((resolve, reject) => {
-        Brands.deleteBrand(item) 
-          .then(response => {
-            resolve(response);
-          })
-          .catch(error => {
-            reject(error);
-          });
-      });
-    },
-    handleUpdate({commit},item){
-      return new Promise((resolve,reject) =>{
-        Brands.handleUpdate(item)
-        .then(res =>{
-          resolve(res)
-         
-         
+      Brands.listBrand(data)
+        .then(res => {
+          commit("LIST_CATEGORY", res.data.result);
+          commit("set_totalData", res.data.meta.total);
+          resolve(res);
         })
-        .catch(error =>{
-          reject(error)
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  deleteBrand({ commit }, item) {
+    return new Promise((resolve, reject) => {
+      Brands.deleteBrand(item)
+        .then(response => {
+          resolve(response);
         })
-      })
-    }
-}
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  handleUpdate({ commit }, item) {
+    return new Promise((resolve, reject) => {
+      Brands.handleUpdate(item)
+        .then(res => {
+          resolve(res);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  }
+};
 
 export default {
-    namespaced: true,
-    state,
-    mutations,
-    actions,
-    getters
-}
-
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
+};
